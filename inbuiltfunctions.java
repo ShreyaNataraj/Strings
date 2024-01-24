1cb78d934416f7c2ad851b79b65cf85fb234fb4b
Shreya N <nshreya094@gmail.com>
2024-01-24 20:48:30 +0530

    inbuilt functions

---
+++ inbuiltfunctions.java
@@ -0,0 +1,11 @@
+public class inbuiltfunctions {
+  public static void main(String[] args) {
+    String str1= "Tony";
+    String str2 = "Tony";
+    String str3 = "Helloworld";
+    System.out.println(str1.equals(str2));
+    System.out.println(str3.substring(0,5));
+    
+
+  }
+}
